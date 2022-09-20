533d4b398a29d6a565e6acd751b42607d73df1cd
abhijeetbh083 <abhijeetbh083@gmail.com>
2022-09-20 22:45:52 +0530

    added fuction

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def main():
   return "Welcome!"
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
 if __name__ == "__main__":
-  app.run()
+  app.run(host="0.0.0.0")
