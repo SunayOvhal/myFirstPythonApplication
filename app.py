76b99eeedf94eee9462d4c21cf5c99572e42e4b2
sunayovhal2007@gmail.com <sunayovhal2007@gmail.com>
2022-09-20 23:06:32 +0530

    added post method

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request, redirect
+import requests
 app = Flask(__name__)
 @app.route("/")
 def main():
@@ -6,5 +8,14 @@ def main():
 @app.route('/success/<name>')
 def success(name):
    return 'welcome %s' % name
+@app.route('/postMethod',methods = ['POST', 'GET'])
+def postMethod():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return 'postcall'
+# return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
